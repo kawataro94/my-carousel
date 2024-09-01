0e8defb57c96733d7b0bb03576b862684ed9f81c
kawataro94 <heneix.s@gmail.com>
2024-09-01 10:18:09 +0900

    add carousel view

--- app/routes/carousel.tsx
+++ app/routes/carousel.tsx
@@ -1,5 +1,13 @@
 import type { MetaFunction } from "@remix-run/react";
 import { Link } from "@remix-run/react";
+import { Card, CardContent } from "~/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Carousel View" }];
@@ -12,6 +20,32 @@ export default function CarouselView() {
       <Link to="/" className="no-underline hover:underline text-blue-600">
         Go to back
       </Link>
+      <div className="flex justify-center">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-sm"
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-3xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 }
